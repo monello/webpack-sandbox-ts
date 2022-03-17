6710a0158800e57f0a773700ff75d28f3245d113
Morné Louw <louw.morne@gmail.com>
2022-03-17 12:14:42 +0200

    A basic example of creating a TS file and compiling it to JS

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
-console.log("Hello form index.ts");
+var _a;
+const component = () => {
+    const element = document.createElement('div');
+    element.innerHTML = "Hello world!<br>Learning webpack";
+    return element;
+};
+(_a = document.getElementById('app')) === null || _a === void 0 ? void 0 : _a.insertAdjacentElement('afterbegin', component());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello form index.ts");
+const component = () => {
+    const element = document.createElement('div');
+    element.innerHTML = "Hello world!<br>Learning webpack";
+    return element;
+}
+
+document.getElementById('app')?.insertAdjacentElement('afterbegin', component());
